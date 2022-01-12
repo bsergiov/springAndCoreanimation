e21f0754f87262934c8763b5299483c17e1790b9
bsergiov <81264661+bsergiov@users.noreply.github.com>
2022-01-12 23:34:23 +0200

    added show parametrs on screen

--- SpringAndCoreAnimation/ViewControllers/ViewController.swift
+++ SpringAndCoreAnimation/ViewControllers/ViewController.swift
@@ -28,25 +28,38 @@ class ViewController: UIViewController {
     private var nextAnimationIndex = 0
     private var nextCurveIndex = 0
     
-    
     // MARK: - Life Cicle
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        animationButton.titleLabel?.text = "Run \(animations[nextAnimationIndex].rawValue)"
     }
     
     // MARK: - IB Action
     @IBAction func changedAnimation(_ sender: UIButton) {
         let parametrAnimation = ParamerAnimationModel.getParametrAnimation()
         
+        let animation = animations[nextCurveIndex].rawValue
+        let curve = curves[nextCurveIndex].rawValue
+        
+        let delay = CGFloat(parametrAnimation.delay)
+        let force = CGFloat(parametrAnimation.force)
+        let duration = CGFloat(parametrAnimation.duration)
+        
         sampleSprinView.animation = animations[nextAnimationIndex].rawValue
         sampleSprinView.curve = curves[nextCurveIndex].rawValue
-        sampleSprinView.delay = CGFloat(parametrAnimation.delay)
-        sampleSprinView.force = CGFloat(parametrAnimation.force)
-        sampleSprinView.duration = CGFloat(parametrAnimation.duration)
+        sampleSprinView.delay = delay
+        sampleSprinView.force = force
+        sampleSprinView.duration = duration
         
         sampleSprinView.animate()
         setupNextAnimation()
+        
+        presetLabel.text = "Preset: \(animation)"
+        curveLabel.text = "Curve: \(curve)"
+        
+        forceLabel.text = "Force: \(Float(round(100 * force) / 100))"
+        durationLabel.text = "Duration: \(Float(round(100 * duration) / 100))"
+        delayLabel.text = "Delay: \(Float(round(100 * delay) / 100))"
     }
     
 
